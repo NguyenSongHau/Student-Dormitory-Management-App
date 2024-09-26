53ce3f4a446bab180cce60d688ff6ef460952496
NguyenSongHau <nguyensonghau27072003@gmail.com>
2024-09-27 03:52:59 +0700

    fix Signup design

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
-import { Text } from 'react-native';
-import Splash from './assets/Components/Common/Splash';
 import Providers from './assets/Navigation/Providers';
-import SignIn from './assets/Screens/Auth/SignIn';
 
 export default function App() {
   return (
-    // <SignIn />
-    <Providers/>
+    <Providers />
   );
 }
